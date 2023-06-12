44f56581a775a72a78277aa427edc3174836c171
obertus <damiencharles1984@gmail.com>
2023-06-12 14:55:52 +0200

    Ajout propType

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import style from "./Button.module.css"
+import PropTypes from 'prop-types'
+
 
 const Button = (props) => {
     console.log("props : ", props)
     return (
         <button className={style.Button}>
-            {undefined !== props.children ?
-                props.children :
-                props.text}
+            {props.children}
         </button>
     );
 }
+Button.propTypes = {
+    children: PropTypes.any.isRequired
+}
+
 export default Button
