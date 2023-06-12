f066635fe04358be1050308d4e5f85e199b5a04d
obertus <damiencharles1984@gmail.com>
2023-06-12 14:23:12 +0200

    Utilisation Css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Button></Button>
+      <Button></Button>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import style from "./Button.module.css"
 
 const Button=(props)=>{
-    return <button>Benjamin</button>
+    return <button className={style.Button}>Benjamin</button>
 }
 export default Button
