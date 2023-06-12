9965b6181c506b4a5eeaf26cbe974841839e1309
obertus <damiencharles1984@gmail.com>
2023-06-12 14:34:33 +0200

    Variabilisation texte

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import style from "./Button.module.css"
 
 const Button=(props)=>{
-    return <button className={style.Button}>Benjamin</button>
+    console.log("props : ", props)
+    return <button className={style.Button}>{props.text}</button>
 }
 export default Button
