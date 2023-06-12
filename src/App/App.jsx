4831655147b73db75193e927bfa28e59e9067bed
obertus <damiencharles1984@gmail.com>
2023-06-12 15:19:56 +0200

    AJout aaction buton

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,8 +6,8 @@ import Button from './components/ui/Button/Button';
 function App() {
   return (
     <div className="App">
-      <Button text="cancel"></Button>
-      <Button><img src='/img/ok.jpg'/></Button>
+      <Button><img src='/img/ok.jpg'/> ok</Button>
+      <Button>cancel</Button>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -6,13 +6,24 @@ import PropTypes from 'prop-types'
 const Button = (props) => {
     console.log("props : ", props)
     return (
-        <button className={style.Button}>
+        <button type={props.type} className={style.Button} onClick={(evt) => {
+            if (undefined !== props.onClick && typeof props.onClick === 'function') {
+                props.onClick();
+            }
+        }}>
             {props.children}
         </button>
     );
 }
 Button.propTypes = {
-    children: PropTypes.any.isRequired
+    children: PropTypes.any.isRequired,
+    type: PropTypes.oneOf(['button', 'reset', 'submit']).isRequired,
+    onClick: PropTypes.func,
+}
+
+Button.defaultProps = {
+    type: 'button',
+    onClick: () => { console.log('hello')}
 }
 
 export default Button
