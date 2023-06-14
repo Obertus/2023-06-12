946c74999f9f7ee67e5c4aaf856f13b78e349ad5
obertus <damiencharles1984@gmail.com>
2023-06-14 12:05:06 +0200

    mise en place db + fetch + save

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,6 +1,6 @@
 import {configureStore,combineReducers} from '@reduxjs/toolkit'
-import currentReducer, { update } from './currentSlice'
-import ressourcesReducer from './ressourcesSlice'
+import currentReducer, { saveCurrent, update } from './currentSlice'
+import ressourcesReducer, { fetchAllRessources } from './ressourcesSlice'
 import {emptyMeme} from 'orsys-tjs-meme'
 const store=configureStore({
     reducer:combineReducers({
@@ -16,4 +16,21 @@ const store=configureStore({
 //  store.dispatch(update(emptyMeme))
 // store.dispatch({type:'current/update', payload:{titre:'coucou'}})
 // store.dispatch({type:'current/clear'})
+// store.dispatch(fetchAllRessources())
+// store.dispatch(saveCurrent({
+//     titre: "titi",
+//     text: "",
+//     x: 0,
+//     y: 20,
+//     fontWeight: "500",
+//     fontSize: 20,
+//     underline: false,
+//     italic: true,
+//     imageId: -1,
+//     color: "#000000",
+//     frameSizeX: 0,
+//     frameSizeY: 0,
+//     id: 9
+//   }))
+
 export default store
