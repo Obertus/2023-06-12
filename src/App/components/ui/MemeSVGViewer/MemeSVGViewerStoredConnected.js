3e61315ebebd15114b9b8a6e62dcef72848fdd68
obertus <damiencharles1984@gmail.com>
2023-06-14 17:10:35 +0200

    Retrait warning useSelector

--- src/App/components/ui/MemeSVGViewer/MemeSVGViewerStoredConnected.js
+++ src/App/components/ui/MemeSVGViewer/MemeSVGViewerStoredConnected.js
@@ -2,17 +2,15 @@ import { MemeSVGViewer } from 'orsys-tjs-meme'
 import { useSelector } from 'react-redux'
 
 export const MemeSVGViewerStoredConnected = (props) => {
-    const storeProps = useSelector(storeState => {
-        return {
-            meme: storeState.current, image: storeState.ressources.images.find(img => {
-                return img.id === storeState.current.imageId
-            })
-        }
-    })
+    const storePropsCurrent = useSelector(s => s.current)
+
+    const storePropsImage = useSelector(s => s.ressources.images)
+
 
     return (
         <MemeSVGViewer
             {...props}
-            {...storeProps}
+            meme={storePropsCurrent}
+            image={storePropsImage.find(img => {return img.id === storePropsCurrent.imageId})}
         />)
 }
