a3d7d27da11b29c8b06b4765b3081fe71f85d231
obertus <damiencharles1984@gmail.com>
2023-06-14 09:21:20 +0200

    dispatch store action

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,13 +1,19 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
-import currentReducer from './currentSlice'
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
+import currentReducer, { update } from './currentSlice'
 import ressourcesReducer from './ressourcesSlice'
-
-const store = configureStore({
-    reducer: combineReducers({
-        ressources: ressourcesReducer,
-        current: currentReducer
+import {emptyMeme} from 'orsys-tjs-meme'
+const store=configureStore({
+    reducer:combineReducers({
+        ressources:ressourcesReducer,
+        current:currentReducer
     }),
-    devTools: true
+    devTools:true
 })
-
+// store.subscribe(()=>{
+//     console.trace(store.getState())
+// })
+// store.dispatch({type:'toto'})
+//  store.dispatch(update(emptyMeme))
+// store.dispatch({type:'current/update', payload:{titre:'coucou'}})
+// store.dispatch({type:'current/clear'})
 export default store
